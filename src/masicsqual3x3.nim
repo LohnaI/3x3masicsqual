2995ca66eb502218f4119b92a0deb19e2ac3f605
LohanI <hks879@naver.com>
2021-11-27 22:58:51 +0900

    study

--- src/masicsqual3x3.nim
+++ src/masicsqual3x3.nim
@@ -5,3 +5,14 @@ var d3 = 0
 for _ in 0 .. 2:
   d1 += 1
   echo d1, " ", d2, " ", d3
+
+
+# 키워드 (keyword)
+# 연산자 (operator, "보통" 특수 기호로 이루어
+# 타입 (형, type) 데이터의 크기와 구조를 나타냄
+# 변수 (variable)
+# 함수 (function, procedure, method)
+# └> 매개변수 (argument, paramenter)
+
+# expression (표현) 어떤 값이 결과로 도출 됨
+# startement (문) 걍 실행만 됨
